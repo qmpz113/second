3658478b0476fa90ae6beaebfc36d6df76f2a510
qmpz113 <nguyenducthinh1612@gmail.com>
2016-09-02 22:43:12 +0700

    Three_site

---
+++ shopping_cart.php
@@ -0,0 +1,4 @@
+<?php include_once './template/head.php'; ?>
+<?php include_once './template/header.php'; ?>
+<?php include_once './template/shopping_cart.php'; ?>
+<?php include_once './template/footer.php'; ?>
---
+++ template/shopping_cart.php
@@ -0,0 +1,181 @@
+<div class="shopping-cart">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="title-cart">
+                    <h1>Shopping Cart</h1>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="title-item-cart">
+                    <h4>Items  (2)</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="cart-products">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="img-cart">
+                                <img src="img/2532594623_6_1_1.png"/>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="info-cart">
+                                <h4>Slim Shorts</h4>
+                                <p>ID# 318931<br/>
+                                    Size: Small<br/>
+                                    Color: Varsity Orange</p>
+                                <button>Edit</button>
+                                <button>Remove</button>
+                                <h3>IN STOCK</h3>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="price-one">
+                                <ul>
+                                    <li><p>540.000 VND</p></li>
+                                    <li><p>x</p></li>
+                                    <li><input type="number" min="1" value="1"/></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="price-one-product">
+                                <h4>540.000 VND</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="cart-products">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="img-cart">
+                                <img src="img/2532594623_6_1_1.png"/>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="info-cart">
+                                <h4>Slim Shorts</h4>
+                                <p>ID# 318931<br/>
+                                    Size: Small<br/>
+                                    Color: Varsity Orange</p>
+                                <button>Edit</button>
+                                <button>Remove</button>
+                                <h3>IN STOCK</h3>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="price-one">
+                                <ul>
+                                    <li><p>540.000 VND</p></li>
+                                    <li><p>x</p></li>
+                                    <li><input type="number" min="1" value="2"/></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="price-one-product">
+                                <h4>1.080.000 VND</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="cost">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="title-cost">
+                                <h4>Estimated Total</h4>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="title-cost">
+                                <h5>(3 Items)</h5>
+                            </div>
+                        </div>
+                        <div class="col-md-offset-3 col-md-3">
+                            <div class="all-cost">
+                                <h4>1.620.000 VND</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="check-out">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="btn-check-out text-center">
+                                <button>Check Out</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-10">
+                <div class="might-like">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="title-like text-center">
+                                <h4>Like the above? You might like these too...</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="img-might-like margin-l0">
+                                <img src="img/2590591712_6_1_1.png"/>
+                            </div>
+                            <div class="item-might-like margin-l0">
+                                <p>Item 1<br/>
+                                    500.000 VND</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="img-might-like">
+                                <img src="img/2590591712_6_1_1.png"/>
+                            </div>
+                            <div class="item-might-like">
+                                <p>Item 1<br/>
+                                    500.000 VND</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="img-might-like">
+                                <img src="img/2590591712_6_1_1.png"/>
+                            </div>
+                            <div class="item-might-like">
+                                <p>Item 1<br/>
+                                    500.000 VND</p>
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="img-might-like margin-r0" >
+                                <img src="img/2590591712_6_1_1.png"/>
+                            </div>
+                            <div class="item-might-like margin-r0">
+                                <p>Item 1<br/>
+                                    500.000 VND</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
