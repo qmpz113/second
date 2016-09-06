6b6fc14ec3a39aa28ea7ed7193f98cae03ad657e
qmpz113 <nguyenducthinh1612@gmail.com>
2016-09-06 23:47:36 +0700

    resposive2

--- template/header.php
+++ template/header.php
@@ -32,24 +32,7 @@
                         </div>
                     </div>
                     <div class="col-xs-2">
-                        <div class="mobile-menu-service">
-                            <ul>
-                                <li class="dropdown">
-                                    <a class="dropdown-toggle" data-toggle="dropdown"><img src="img/menu_mobile.jpg"/></a>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="#">All</a></li>
-                                        <li><a href="#">New</a></li>
-                                        <li><a href="#">Outerwear</a></li> 
-                                        <li><a href="#">Tops</a></li> 
-                                        <li><a>Shirts</a></li>
-                                        <li><a>Dresses & Jumpsuits</a></li>
-                                        <li><a>Trousers</a></li>
-                                        <li><a>Shorts</a></li>
-                                        <li><a>Jeans</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
+                        
                     </div>
                     <div class="col-md-7 col-sm-9 col-xs-12">
                         <div class="menu-service">
@@ -82,7 +65,24 @@
                                 <li><a>Jeans</a></li>
                             </ul>
                         </div>
-                        
+                        <div class="mobile-menu-service">
+                            <ul>
+                                <li class="dropdown">
+                                    <a class="dropdown-toggle" data-toggle="dropdown"><img src="img/menu_mobile.jpg"/></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">All</a></li>
+                                        <li><a href="#">New</a></li>
+                                        <li><a href="#">Outerwear</a></li> 
+                                        <li><a href="#">Tops</a></li> 
+                                        <li><a>Shirts</a></li>
+                                        <li><a>Dresses & Jumpsuits</a></li>
+                                        <li><a>Trousers</a></li>
+                                        <li><a>Shorts</a></li>
+                                        <li><a>Jeans</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
